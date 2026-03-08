1d0416b3a71fbba00cc957c5ca23883dca6d555a
Thireus ☠ <Thireus@users.noreply.github.com>
2026-03-08 21:58:34 +0000

    Revert "Lift limitations of llama-quantize"
    This reverts commit 828510a18a4b6a9e91de894f6bba31af08952a63.

--- ggml/src/ggml-quants.c
+++ ggml/src/ggml-quants.c
@@ -14293,8 +14293,7 @@ static void quantize_row_iq1_s_impl(const float * restrict x, void * restrict vy
     const int      * kmap_q2xs       = iq2_data[gindex].map;
     const uint16_t * kneighbors_q2xs = iq2_data[gindex].neighbours;
 
-    // THIREUS: Lift limitations of llama-quantize
-    //GGML_ASSERT(quant_weights   && "missing quantization weights");
+    GGML_ASSERT(quant_weights   && "missing quantization weights");
     GGML_ASSERT(kgrid_q2xs      && "forgot to call ggml_quantize_init()?");
     GGML_ASSERT(kmap_q2xs       && "forgot to call ggml_quantize_init()?");
     GGML_ASSERT(kneighbors_q2xs && "forgot to call ggml_quantize_init()?");
