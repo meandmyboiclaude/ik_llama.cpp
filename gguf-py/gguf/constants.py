d6e60c30393d3fe00c6e220c721079fc1adb7788
Thireus ☠ <Thireus@users.noreply.github.com>
2026-03-11 21:27:37 +0000

    Revert "Merge branch 'th/quantize_individual_tensors' of https://github.com/Thireus/ik_llama.cpp into th/quantize_individual_tensors"
    This reverts commit 09936cb330854c4c7b132fcaeab41ac107b5c1b8, reversing
    changes made to 5a3d2401987cae83ef0fbea2db843993e715f86c.

--- gguf-py/gguf/constants.py
+++ gguf-py/gguf/constants.py
@@ -186,11 +186,6 @@ class Keys:
         TYPE       = "adapter.type"
         LORA_ALPHA = "adapter.lora.alpha"
 
-    class IMatrix:
-        CHUNK_COUNT = "imatrix.chunk_count"
-        CHUNK_SIZE  = "imatrix.chunk_size"
-        DATASETS    = "imatrix.datasets"
-
 #
 # recommended mapping of model tensor names for storage in gguf
 #
@@ -199,7 +194,6 @@ class Keys:
 class GGUFType:
     MODEL   = "model"
     ADAPTER = "adapter"
-    IMATRIX = "imatrix"
 
 
 class MODEL_ARCH(IntEnum):
